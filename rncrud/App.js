5a6c993b09c2c0e76491e85ddb4b5ef7524cf10f
Faitly <fty.57@alu.ufc.br>
2021-11-02 12:39:08 -0300

    102. Criando Arquivo de Usuários

--- rncrud/App.js
+++ rncrud/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Button} from 'react-native'
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import UserList from './src/views/UserList';
---
+++ rncrud/src/data/users.js
@@ -0,0 +1,26 @@
+export default [
+     {
+          id: 1,
+          name: "Álisson Véras",
+          email: "fty.57@alu.ufc.br",
+          avatarUrl: "https://cdn.pixabay.com/photo/2021/10/08/20/28/dwarf-6692467_960_720.png"
+     },
+     {
+          id: 2,
+          name: "Manuella Véras",
+          email: "mvd12555@gmail.com",
+          avatarUrl: "https://cdn.pixabay.com/photo/2021/10/08/20/28/hunter-6692462_960_720.png"
+     },
+     {
+          id: 3,
+          name: "Raimunda Véras",
+          email: "raimunda@gmail.com",
+          avatarUrl: "https://cdn.pixabay.com/photo/2021/10/08/20/28/fighter-6692465_960_720.png"
+     },
+     {
+          id: 4,
+          name: "João Vinicius",
+          email: "joaovinicius@gmail.com",
+          avatarUrl: "https://cdn.pixabay.com/photo/2021/10/08/20/29/ninja-6692470_960_720.png"
+     }
+]
