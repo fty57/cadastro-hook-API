b5e86b0d89d947e0908c1cade0152a7041c8f7d4
Faitly <fty.57@alu.ufc.br>
2021-11-02 13:11:35 -0300

    104. Lista de Usuários #02

--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -1,23 +1,57 @@
 import React from "react"
 import { View, Text, FlatList } from "react-native"
-import { ListItem } from "react-native-elements"
 import users from "../data/users"
+import { ListItem, Button, Icon } from 'react-native-elements';
 
 export default props => {
-     function getUserItem({ item : user }) {
+
+     function confirmUserDeletion(user){
+          Alert.alert("Excluir Usuário", "Deseja excluir o usuário?", [
+               {
+                    text: "Sim",
+                    onPress(){
+                         console.warn("delete" + user.id)
+                    }
+               },
+               {
+                    text: "Não"
+               }
+          ])
+     }
+
+     function getAction(user) {
+          return (
+               <>
+                    <Button
+                         onPress={() => props.navigation.navigate("UserForm", user)}
+                         type="clear"
+                         icon={<Icon name="edit" size={25} color="orange" />}
+                    />
+
+                    <Button
+                         onPress={() => confirmUserDeletion(user)}
+                         type="clear"
+                         icon={<Icon name="delete" size={25} color="red" />}
+                    />
+               </>
+          )
+     }
+
+     function getUserItem({ item: user }) {
           return (
                <ListItem
-                    leftAvatar={{source: {uri: user.avatarUrl}}}
+                    leftAvatar={{ source: { uri: user.avatarUrl } }}
                     key={user.id}
                     title={user.name}
                     subtitle={user.email}
                     bottomDivider
-                    onPress={()=> props.navigation.navigate("UserForm")}
+                    rightElement={getAction(user)}
+                    onPress={() => props.navigation.navigate("UserForm", user)}
                />
           )
      }
 
-     return(
+     return (
           <View>
                <FlatList
                     keyExtractor={user => user.id.toString()}
