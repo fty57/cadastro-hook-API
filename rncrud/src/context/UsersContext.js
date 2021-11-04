d88cdf4488c64d4b1cb9305c446f469011c0db6c
Faitly <fty.57@alu.ufc.br>
2021-11-04 16:19:36 -0300

    111. useReducer #03

--- rncrud/src/context/UsersContext.js
+++ rncrud/src/context/UsersContext.js
@@ -1,22 +1,37 @@
-import React, {createContext, useReducer } from 'react'
+import React, { createContext, useReducer } from 'react'
 import users from "../data/users"
 
 const initialState = { users }
 const UsersContext = createContext({})
 
-const actions = { 
+const actions = {
+     createUser(state, action) {
+          const user = action.payload
+          user.id = Math.random()
+          return {
+               ...state,
+               users: [...state.users, user],
+          }
+     },
+     updateUser(){
+          const updated = action.payload
+          return {
+               ...state,
+               users: state.users.map( u => u.id === updated.id ? updated : u)
+          }
+     },
      deleteUser(state, action) {
           const user = action.payload
-               return {
-                    //...state,
-                    users: state.users.filter(u => u.id !== user.id)
-               }
+          return {
+               ...state,
+               users: state.users.filter(u => u.id !== user.id)
+          }
      }
 }
 
 export const UsersProvider = props => {
 
-     function reducer(state, action){
+     function reducer(state, action) {          
           const fn = actions[action.type]
           return fn ? fn(state, action) : state
      }
--- rncrud/src/views/UserForm.js
+++ rncrud/src/views/UserForm.js
@@ -3,6 +3,7 @@ import {Text , TextInput, View, StyleSheet, Button} from "react-native"
 
 export default ({route, navigation}) => {
      const [user, setUser] = useState(route.params ? route.params : {} )
+     const {dispatch} = useContext(UsersContext)
      return(
           <View style={styles.form}>
                <Text>Name</Text>
@@ -31,6 +32,10 @@ export default ({route, navigation}) => {
                <Button
                     title="Salvar"
                     onPress={() => {
+                         dispatch({
+                              type: user.id? "updateUser" : "createUser",
+                              payload: user, 
+                         })
                          navigation.goBack();
                     }}
                />
