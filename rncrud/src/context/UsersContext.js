fd3f38cbb1774cb7f12d676b6be1695b5748043e
Faitly <fty.57@alu.ufc.br>
2021-11-04 16:02:01 -0300

    109. useReducer #01

--- rncrud/src/context/UsersContext.js
+++ rncrud/src/context/UsersContext.js
@@ -1,8 +1,27 @@
-import React, {createContext } from 'react'
+import React, {createContext, useReducer } from 'react'
 import users from "../data/users"
 
+const initialState = { users }
 const UsersContext = createContext({})
 
-export const UsersProvider 
+export const UsersProvider = props => {
+
+     function reducer(state, action){
+          console.warn(action)
+          return state
+     }
+
+     const [state, dispatch] = useReducer(reducer, initialState)
+
+     return (
+          <UsersContext.Provider value={{ state, dispatch }}>
+               {props.children}
+          </UsersContext.Provider>
+     )
+}
 
 export default UsersContext
+
+// O objetivo do reducer é evoluir o estado atual
+// Ele recebe o estado e também recebe uma ação
+// Sempre retornando o estado evoluido
